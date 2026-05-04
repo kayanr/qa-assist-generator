e9bbf5889e52218c3d122c99c33cb988a1f35e1f
Kayan Reid <kayan.reid@gmail.com>
2026-05-04 13:06:43 -0400

    feat: add TestCaseGeneratorService

---
+++ src/main/java/com/qaassist/generator/engine/TestCaseGeneratorService.java
@@ -0,0 +1,19 @@
+package com.qaassist.generator.engine;
+
+import java.util.List;
+
+import com.qaassist.generator.engine.model.TestCase;
+import com.qaassist.generator.engine.model.TestCaseRequest;
+import com.qaassist.generator.engine.registry.TemplateRegistry;
+
+public class TestCaseGeneratorService {
+    private TemplateRegistry registry;
+
+    public TestCaseGeneratorService(TemplateRegistry registry) {
+        this.registry = registry;
+    }
+
+    public List<TestCase> generate(TestCaseRequest request) {
+        return registry.getTemplate(request.getFeatureType()).generate(request);
+    }
+}
